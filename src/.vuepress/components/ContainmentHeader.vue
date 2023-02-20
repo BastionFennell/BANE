19302994df1424300c01eeacb71f91694293b04d
Bastion Fennell <bastion@brighthire.ai>
2023-02-19 23:42:46 -0600

    Reorganize quickstart

--- src/.vuepress/components/ContainmentHeader.vue
+++ src/.vuepress/components/ContainmentHeader.vue
@@ -42,31 +42,31 @@
           </div>
       </div>
       <div class="quick-view">
-          <div class="top" :class="containment">
+          <div class="top none">
               <div class="diamond">
                   <svg stroke="black" width="75" height="75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650"><g><path id="svg_2" d="m25.30386,325.00001l299.98623,-299.9863l299.40604,299.9863l-299.40604,299.98629l-299.98623,-299.98629z" fill="currentColor" /></g></svg>
               </div>
-              <div class="icon"><i :class="getIconClass(containment)"></i></div>
+              <div class="icon"><i :class="getIconClass(securityLevel)"></i></div>
           </div>
           <div class="middle">
-              <div class="left" :class="disruption">
+              <div class="left"  :class="containment">
+                  <div class="diamond">
+                      <svg stroke="black" width="75" height="75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650"><g><path id="svg_2" d="m25.30386,325.00001l299.98623,-299.9863l299.40604,299.9863l-299.40604,299.98629l-299.98623,-299.98629z" fill="currentColor" /></g></svg>
+                  </div>
+                  <div class="icon"><i :class="getIconClass(containment)"></i></div>
+              </div>
+              <div class="right" :class="disruption">
                   <div class="diamond">
                       <svg stroke="black" width="75" height="75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650"><g><path id="svg_2" d="m25.30386,325.00001l299.98623,-299.9863l299.40604,299.9863l-299.40604,299.98629l-299.98623,-299.98629z" fill="currentColor" /></g></svg>
                   </div>
                   <div class="icon"><i :class="getIconClass(disruption)"></i></div>
               </div>
-              <div class="right" :class="risk">
-                  <div class="diamond">
-                      <svg stroke="black" width="75" height="75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650"><g><path id="svg_2" d="m25.30386,325.00001l299.98623,-299.9863l299.40604,299.9863l-299.40604,299.98629l-299.98623,-299.98629z" fill="currentColor" /></g></svg>
-                  </div>
-                  <div class="icon"><i :class="getIconClass(risk)"></i></div>
-              </div>
           </div>
-          <div class="bottom">
-              <div class="diamond none">
+          <div class="bottom" :class="risk">
+              <div class="diamond">
                   <svg stroke="black" width="75" height="75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650"><g><path id="svg_2" d="m25.30386,325.00001l299.98623,-299.9863l299.40604,299.9863l-299.40604,299.98629l-299.98623,-299.98629z" fill="currentColor" /></g></svg>
-                  <div class="icon"><i :class="getIconClass(securityLevel)"></i></div>
               </div>
+              <div class="icon"><i :class="getIconClass(risk)"></i></div>
           </div>
       </div>
   </div>
